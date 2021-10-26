1e558d743b2a2d790ca1387f8f62abc6f8834d88
marco <marcoantoniomamanichura@gmail.com>
2021-10-26 19:45:25 -0400

    diseno

--- DinoM/Descuentos/Descuentos.Designer.vb
+++ DinoM/Descuentos/Descuentos.Designer.vb
@@ -25,11 +25,18 @@ Partial Class Descuentos
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.GroupPanel1 = New DevComponents.DotNetBar.Controls.GroupPanel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
         Me.grdetalle = New Janus.Windows.GridEX.GridEX()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.GroupPanel2 = New DevComponents.DotNetBar.Controls.GroupPanel()
+        Me.GridEX1 = New Janus.Windows.GridEX.GridEX()
+        Me.btnNuevo = New DevComponents.DotNetBar.ButtonX()
+        Me.btnGrabar = New DevComponents.DotNetBar.ButtonX()
         Me.Panel2.SuspendLayout()
         Me.GroupPanel1.SuspendLayout()
         CType(Me.grdetalle, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        Me.GroupPanel2.SuspendLayout()
+        CType(Me.GridEX1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
@@ -38,17 +45,18 @@ Partial Class Descuentos
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(933, 75)
+        Me.Panel1.Size = New System.Drawing.Size(1066, 75)
         Me.Panel1.TabIndex = 0
         '
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.Color.White
+        Me.Panel2.Controls.Add(Me.GroupPanel2)
         Me.Panel2.Controls.Add(Me.GroupPanel1)
         Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel2.Location = New System.Drawing.Point(0, 75)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(933, 443)
+        Me.Panel2.Size = New System.Drawing.Size(1066, 443)
         Me.Panel2.TabIndex = 0
         '
         'GroupPanel1
@@ -60,9 +68,9 @@ Partial Class Descuentos
         Me.GroupPanel1.DisabledBackColor = System.Drawing.Color.Empty
         Me.GroupPanel1.Dock = System.Windows.Forms.DockStyle.Right
         Me.GroupPanel1.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupPanel1.Location = New System.Drawing.Point(552, 0)
+        Me.GroupPanel1.Location = New System.Drawing.Point(586, 0)
         Me.GroupPanel1.Name = "GroupPanel1"
-        Me.GroupPanel1.Size = New System.Drawing.Size(381, 443)
+        Me.GroupPanel1.Size = New System.Drawing.Size(480, 443)
         '
         '
         '
@@ -95,27 +103,109 @@ Partial Class Descuentos
         Me.GroupPanel1.TabIndex = 0
         Me.GroupPanel1.Text = "Detalle Descuento"
         '
-        'Panel3
-        '
-        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel3.Location = New System.Drawing.Point(0, 0)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(375, 165)
-        Me.Panel3.TabIndex = 0
-        '
         'grdetalle
         '
         Me.grdetalle.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.grdetalle.Location = New System.Drawing.Point(0, 165)
+        Me.grdetalle.Location = New System.Drawing.Point(0, 192)
         Me.grdetalle.Name = "grdetalle"
-        Me.grdetalle.Size = New System.Drawing.Size(375, 245)
+        Me.grdetalle.Size = New System.Drawing.Size(474, 218)
         Me.grdetalle.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.btnGrabar)
+        Me.Panel3.Controls.Add(Me.btnNuevo)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(474, 192)
+        Me.Panel3.TabIndex = 0
+        '
+        'GroupPanel2
+        '
+        Me.GroupPanel2.CanvasColor = System.Drawing.SystemColors.Control
+        Me.GroupPanel2.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007
+        Me.GroupPanel2.Controls.Add(Me.GridEX1)
+        Me.GroupPanel2.DisabledBackColor = System.Drawing.Color.Empty
+        Me.GroupPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupPanel2.Location = New System.Drawing.Point(0, 0)
+        Me.GroupPanel2.Name = "GroupPanel2"
+        Me.GroupPanel2.Size = New System.Drawing.Size(586, 443)
+        '
+        '
+        '
+        Me.GroupPanel2.Style.BackColor = System.Drawing.Color.Black
+        Me.GroupPanel2.Style.BackColor2 = System.Drawing.Color.DarkBlue
+        Me.GroupPanel2.Style.BackColorGradientAngle = 90
+        Me.GroupPanel2.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderBottomWidth = 1
+        Me.GroupPanel2.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder
+        Me.GroupPanel2.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderLeftWidth = 1
+        Me.GroupPanel2.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderRightWidth = 1
+        Me.GroupPanel2.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderTopWidth = 1
+        Me.GroupPanel2.Style.CornerDiameter = 4
+        Me.GroupPanel2.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded
+        Me.GroupPanel2.Style.Font = New System.Drawing.Font("Calibri", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupPanel2.Style.TextColor = System.Drawing.Color.White
+        '
+        '
+        '
+        Me.GroupPanel2.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        '
+        '
+        '
+        Me.GroupPanel2.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.GroupPanel2.TabIndex = 1
+        Me.GroupPanel2.Text = "LISTADO DE PRODUCTOS"
+        '
+        'GridEX1
+        '
+        Me.GridEX1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GridEX1.Location = New System.Drawing.Point(0, 0)
+        Me.GridEX1.Name = "GridEX1"
+        Me.GridEX1.Size = New System.Drawing.Size(580, 413)
+        Me.GridEX1.TabIndex = 0
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.btnNuevo.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground
+        Me.btnNuevo.Font = New System.Drawing.Font("Calibri", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNuevo.Image = Global.DinoM.My.Resources.Resources.add
+        Me.btnNuevo.ImageFixedSize = New System.Drawing.Size(25, 25)
+        Me.btnNuevo.ImageTextSpacing = 5
+        Me.btnNuevo.Location = New System.Drawing.Point(47, 127)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(135, 47)
+        Me.btnNuevo.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2013
+        Me.btnNuevo.TabIndex = 0
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.TextColor = System.Drawing.Color.MidnightBlue
+        '
+        'btnGrabar
+        '
+        Me.btnGrabar.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.btnGrabar.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground
+        Me.btnGrabar.Font = New System.Drawing.Font("Calibri", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGrabar.Image = Global.DinoM.My.Resources.Resources.save
+        Me.btnGrabar.ImageFixedSize = New System.Drawing.Size(25, 25)
+        Me.btnGrabar.ImageTextSpacing = 5
+        Me.btnGrabar.Location = New System.Drawing.Point(267, 127)
+        Me.btnGrabar.Name = "btnGrabar"
+        Me.btnGrabar.Size = New System.Drawing.Size(135, 47)
+        Me.btnGrabar.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2013
+        Me.btnGrabar.TabIndex = 1
+        Me.btnGrabar.Text = "Grabar"
+        Me.btnGrabar.TextColor = System.Drawing.Color.MidnightBlue
         '
         'Descuentos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(933, 518)
+        Me.ClientSize = New System.Drawing.Size(1066, 518)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Descuentos"
@@ -123,6 +213,9 @@ Partial Class Descuentos
         Me.Panel2.ResumeLayout(False)
         Me.GroupPanel1.ResumeLayout(False)
         CType(Me.grdetalle, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.GroupPanel2.ResumeLayout(False)
+        CType(Me.GridEX1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -132,4 +225,8 @@ Partial Class Descuentos
     Friend WithEvents GroupPanel1 As DevComponents.DotNetBar.Controls.GroupPanel
     Friend WithEvents grdetalle As Janus.Windows.GridEX.GridEX
     Friend WithEvents Panel3 As Panel
+    Friend WithEvents GroupPanel2 As DevComponents.DotNetBar.Controls.GroupPanel
+    Friend WithEvents GridEX1 As Janus.Windows.GridEX.GridEX
+    Friend WithEvents btnGrabar As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents btnNuevo As DevComponents.DotNetBar.ButtonX
 End Class
